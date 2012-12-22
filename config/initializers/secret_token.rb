a593b5b155a0a928440430ed470e362569f03e16
HR <radio.github@gmail.com>
2012-12-21 18:21:50 -0800

    eol

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = 'a31fa0c21f9355e2b8c838d5d1a69b7a6f939adb0849f714544756fff30514be76b3eed7dd4875e3290f24bd443a37c23955a47124a9f4d25622fddae66d9b59'
